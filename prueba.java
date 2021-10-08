560cc9067ef840616a6ef74893a2acb7b4644dd0
PabloP95 <pablo95piedad@gmail.com>
2021-10-08 11:27:50 +0200

    Primer archivo

---
+++ prueba.java
@@ -0,0 +1,11 @@
+package git;
+
+public class TestClass {
+	private final String message;
+	public TestClass() {
+		this.message = "Hola mundo";
+	}
+	public String getMessage() {
+		return message;
+	}
+}
